dfff17fd2e805accf04e4ff0c4aff1d4f056aee9
Kanaestephe <Kanaestephe@gmail.com>
2023-11-24 15:46:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,7 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-web" {
+module "s3-webapp" {
   source  = "app.terraform.io/estephe-org/s3-web/aws"
-  name   = var.name
-  region = var.region
-  prefix = var.prefix
   version = "1.0.0"
 }
